8bd50b483f24ef422e6a5e5c320ce9ef52dfed90
Hugh Kohl <hgkohl@us.ibm.com>
2018-10-02 18:15:39 -0700

    Second commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// function outputGuess() {
+//     x= document.getElementById("form1").textContent;
+//     document.write(x);
+// }
+
+function outputGuess() {
+    document.getElementById("form1").submit();
+}
