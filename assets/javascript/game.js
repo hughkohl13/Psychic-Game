5d080b4fdaa5b68ff93092dd6b6ee12153817860
Hugh Kohl <hgkohl@us.ibm.com>
2018-10-06 13:45:18 -0700

    third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,24 @@
 //     document.write(x);
 // }
 
+// create array of words
+// choose a word randomly
+// create array of random word
+// for each i in word array display underscore
+// Get user input
+    // if input letter matches any letter in random word array, display that letter in underscore
+    // if input does not match, --1 from score
+
 function outputGuess() {
     document.getElementById("form1").submit();
 }
+
+// defining our quiz objects
+var quizWords = [
+    {q:"Trick or ...", a:"treat"},
+    {q:"", a:""},
+    {q:"", a:""},
+    {q:"", a:""},
+    {q:"", a:""},
+]
+
